ce0f65509fbbc465a7a7d1b020b62c231cabc6d9
BeeMao03 <153680716+BeeMao03@users.noreply.github.com>
2024-01-14 11:31:38 -0300

    Add API search functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current_temperature_value");
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#main_city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = currentTemperature;
+}
+
+function searchCity(city) {
+  let apiKey = "t8be874ao00e969ec72593d742ee985f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search_input");
-  let cityElement = document.querySelector("#main_city");
 
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search_form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Asunción");
